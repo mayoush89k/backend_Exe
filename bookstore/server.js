bec5cc6f6cb022822707f2960351d9cb3346c1a9
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2024-01-01 20:13:06 +0200

    adding cors

--- bookstore/server.js
+++ bookstore/server.js
@@ -3,7 +3,7 @@ import { errorHandler } from './middleware/errorMiddleware.js'
 import bookRoutes from './routes/bookRoutes.js'
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 app.use('/books', bookRoutes)
