0aaed18976ebf43ec21dfea87e500bd3ab368a39
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2024-01-01 20:32:39 +0200

    fixing adding new book

--- bookstore/controllers/bookControllers.js
+++ bookstore/controllers/bookControllers.js
@@ -46,12 +46,14 @@ const createANewBook = (req, res, next) => {
     const id = books.length + 1;
     const newBook = {
       id,
-      title,author,genre,
+      title,
+      author,
+      genre,
       pages: pages ? pages : crypto.randomInt(1000),
       publishedIn: publishedIn ? publishedIn : 1000 + crypto.randomInt(1023),
       description: description ? description : `description ${id}`,
       coverImage,
-      price: price ? price : crypto.randomInt(20) + 0.90,
+      price: price ? price : crypto.randomInt(20) + 0.9,
       reviews: [],
       averageRating: 0,
       readingProgress: 0,
